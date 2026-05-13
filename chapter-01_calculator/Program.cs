4f2dafd1b0b6232c0fb38cbd3bb09940e2b1616f
rkraus-dev <krobert98@outlook.de>
2026-05-13 15:31:05 +0200

    feat: add calculator project structure with step comments

--- chapter-01_calculator/Program.cs
+++ chapter-01_calculator/Program.cs
@@ -1,1 +1,9 @@
-﻿Console.WriteLine("Hello, World!");
+﻿// Step 1 - ask user for two numbers
+
+// Step 2 - ask user for operator
+
+// Step 3 - check if division by zero
+
+// Step 4 - perform calculation 
+
+// Step 5 - output result
