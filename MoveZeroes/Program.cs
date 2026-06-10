ba765d991909bdfdcf776044dd0193e05f47b58f
rkraus-dev <krobert98@outlook.de>
2026-06-10 12:32:42 +0200

    feat: add move zeroes logic

--- MoveZeroes/Program.cs
+++ MoveZeroes/Program.cs
@@ -1,1 +1,26 @@
-﻿
+﻿// Input array
+int[] numbers = { 0, 1, 0, 3, 12 };
+
+Console.WriteLine($"Input:  {string.Join(", ", numbers)}");
+
+int insertPos = 0;
+
+// Move non-zero values to the front
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (numbers[i] != 0)
+    {
+        numbers[insertPos] = numbers[i];
+        insertPos++;
+    }
+}
+
+// Fill remaining positions with zeroes
+for (int i = insertPos; i < numbers.Length; i++)
+{
+    numbers[i] = 0;
+}
+
+
+
+Console.WriteLine($"Output: {string.Join(", ", numbers)}");
