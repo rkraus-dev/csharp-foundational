efe303f3679d27d153c9fb0da5aebb1678186066
rkraus-dev <krobert98@outlook.de>
2026-06-10 11:23:21 +0200

    feat: add main validation logic and output

--- PasswordValidatorExtended/Program.cs
+++ PasswordValidatorExtended/Program.cs
@@ -1,14 +1,14 @@
 ﻿Console.Write("Enter a password: ");
-string userInput = Console.ReadLine() ?? "";
+string userInput = (Console.ReadLine() ?? "").Trim();
 
 // Method for checking the min length of the password
-bool hasMinLength(string password)
+bool HasMinLength(string password)
 {
     return password.Length >= 8;
 }
 
 // Method for checking if a char is Uppercase
-bool hasUpperCase(string password)
+bool HasUpperCase(string password)
 {
     foreach (char c in password)
     {
@@ -21,7 +21,7 @@ bool hasUpperCase(string password)
 }
 
 // Method for checking if a char is lowercase
-bool hasLowerCase(string password)
+bool HasLowerCase(string password)
 {
     foreach (char c in password)
     {
@@ -32,7 +32,7 @@ bool hasLowerCase(string password)
 }
 
 // Method for checking if a char is a digit
-bool hasDigit(string password)
+bool HasDigit(string password)
 {
     foreach (char c in password)
     {
@@ -42,16 +42,38 @@ bool hasDigit(string password)
     return false;
 }
 
-// Method for checking if char is a special char
-bool hasSpecialChar(string password)
+// Method for checking if a char is a special character
+bool HasSpecialChar(string password)
 {
     string specialChars = "!@#$%^&*";
 
-    foreach(char c in password)
+    foreach (char c in password)
     {
-        if(specialChars.Contains(c.ToString()))
+        if (specialChars.Contains(c.ToString()))
             return true;
     }
     return false;
 
 }
+
+// Run all validation checks
+bool hasMinLength = HasMinLength(userInput);
+bool hasLower = HasLowerCase(userInput);
+bool hasUpper = HasUpperCase(userInput);
+bool hasDigit = HasDigit(userInput);
+bool hasSpecial = HasSpecialChar(userInput);
+
+if (!hasMinLength || !hasLower || !hasUpper || !hasDigit || !hasSpecial)
+    Console.WriteLine("Password is invalid!");
+if (!hasMinLength)
+    Console.WriteLine("- Must be at least 8 characters!");
+if (!hasUpper)
+    Console.WriteLine("- Must contain an uppercase letter");
+if (!hasLower)
+    Console.WriteLine("- Must contain an lowercase letter");
+if (!hasDigit)
+    Console.WriteLine("- Must contain a digit");
+if (!hasSpecial)
+    Console.WriteLine("- Must contain a special character (!@#$%^&*)");
+if(hasMinLength && hasLower && hasUpper && hasDigit && hasSpecial)
+    Console.WriteLine("Password is valid!");
