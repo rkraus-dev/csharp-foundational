5be789a5b3e1113e3f92188d34c136f993561612
rkraus-dev <krobert98@outlook.de>
2026-06-10 15:43:35 +0200

    feat: add 2D grid output with sum and max calculation

--- Grid2D/Program.cs
+++ Grid2D/Program.cs
@@ -1,1 +1,31 @@
-﻿Console.WriteLine("Hello, World!");
+﻿// Grid & Variables
+int sum = 0;
+int max = 0;
+int[,] grid =
+{
+    {13,2,3},
+    {4,5,6},
+    {7,8,9}
+
+};
+
+// Output of Grid, Sum and Max
+Console.WriteLine("Grid:");
+for (int i = 0; i < 3; i++)
+{
+    for (int j = 0; j < 3; j++)
+    {
+        // write current cell value
+        Console.Write($"{grid[i, j]} ");
+        // add all numbers from the grid
+        sum += grid[i,j];
+        // check which number is max 
+        if(max < grid[i,j])
+            max = grid[i,j];
+    }
+    Console.WriteLine();
+}
+
+// Console Output
+Console.WriteLine($"Sum: {sum}");
+Console.WriteLine($"Max: {max}");
