49a490401902ef451c39ffec93db697068745438
rkraus-dev <krobert98@outlook.de>
2026-06-10 13:54:18 +0200

    feat: add missing number logic

--- MissingNumber/Program.cs
+++ MissingNumber/Program.cs
@@ -1,1 +1,19 @@
-﻿Console.WriteLine("Hello, World!");
+﻿int[] numbers = { 3, 0, 1 };
+
+// Calculate expected sum
+int expectedSum  = numbers.Length * (numbers.Length + 1) / 2;
+
+// Calculate actual array sum 
+int actualSum = 0;
+for(int i = 0; i < numbers.Length; i++)
+{
+    actualSum += numbers[i];
+
+}
+
+// Calculate missing number
+int missingNumber = expectedSum  - actualSum;
+
+// Output
+Console.WriteLine($"Input:  {string.Join(", ", numbers)}");
+Console.WriteLine($"Missing number: {missingNumber}");
