9a466a3507d291b050eaf2d17016fe70955742e4
rkraus-dev <krobert98@outlook.de>
2026-06-10 10:47:02 +0200

    feat: add password validation methods

--- PasswordValidatorExtended/Program.cs
+++ PasswordValidatorExtended/Program.cs
@@ -1,1 +1,57 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.Write("Enter a password: ");
+string userInput = Console.ReadLine() ?? "";
+
+// Method for checking the min length of the password
+bool hasMinLength(string password)
+{
+    return password.Length >= 8;
+}
+
+// Method for checking if a char is Uppercase
+bool hasUpperCase(string password)
+{
+    foreach (char c in password)
+    {
+        if (char.IsUpper(c))
+            return true;
+
+    }
+
+    return false;
+}
+
+// Method for checking if a char is lowercase
+bool hasLowerCase(string password)
+{
+    foreach (char c in password)
+    {
+        if (char.IsLower(c))
+            return true;
+    }
+    return false;
+}
+
+// Method for checking if a char is a digit
+bool hasDigit(string password)
+{
+    foreach (char c in password)
+    {
+        if (char.IsDigit(c))
+            return true;
+    }
+    return false;
+}
+
+// Method for checking if char is a special char
+bool hasSpecialChar(string password)
+{
+    string specialChars = "!@#$%^&*";
+
+    foreach(char c in password)
+    {
+        if(specialChars.Contains(c.ToString()))
+            return true;
+    }
+    return false;
+
+}
