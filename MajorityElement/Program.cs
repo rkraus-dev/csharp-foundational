70d747a6548fb236252adfffaa023bf48353421a
rkraus-dev <krobert98@outlook.de>
2026-06-11 12:21:57 +0200

    feat: add majority element logic using Dictionary

--- MajorityElement/Program.cs
+++ MajorityElement/Program.cs
@@ -1,1 +1,26 @@
-﻿Console.WriteLine("Hello, World!");
+﻿// Input array
+int[] arr = { 3, 2, 3 };
+
+// Count occurrences of each element
+Dictionary<int, int> dict = new Dictionary<int, int>();
+
+//Print array
+Console.WriteLine($"[{string.Join(", ", arr)}]");
+
+// Populate dictionary with element counts
+foreach (int i in arr)
+{
+    if (dict.ContainsKey(i))
+        dict[i]++;
+    else
+        dict[i] = 1;
+}
+
+// Find element that appears more than n/2 times
+foreach (KeyValuePair<int, int> item in dict)
+{
+    if(item.Value > arr.Length / 2)
+    {
+        Console.WriteLine($"Majority Element: {item.Key}");
+    }
+}
