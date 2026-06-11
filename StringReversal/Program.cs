31af472f9549bb418b45de308f8ab4479bf8e9a2
rkraus-dev <krobert98@outlook.de>
2026-06-11 13:52:15 +0200

    feat: add string reversal logic

--- StringReversal/Program.cs
+++ StringReversal/Program.cs
@@ -1,1 +1,11 @@
-﻿Console.WriteLine("Hello, World!");
+﻿// User Input 
+Console.Write("Enter a String: ");
+string userInput = (Console.ReadLine() ?? "").Trim();
+
+Console.Write("Reversed: ");
+
+// Loop to reverse the user input
+for (int i = userInput.Length; i > 0; i--)
+{
+    Console.Write(userInput[i -1]);
+}
