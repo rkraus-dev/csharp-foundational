95e1a84a29e058de48a875cadeac34ed5a139517
rkraus-dev <krobert98@outlook.de>
2026-06-11 11:32:14 +0200

    fix: save Program.cs changes

--- FibonacciIterative/Program.cs
+++ FibonacciIterative/Program.cs
@@ -1,13 +1,16 @@
-﻿//...
+﻿//Print and read user input
 Console.Write("How many Fibonacci numbers?: ");
 int.TryParse(Console.ReadLine(), out int input);
 
+// Starting values for Fibonacci sequence
 int num1 = 0;
 int num2 = 1;
 
+// Print output header
 Console.WriteLine();
 Console.Write("Fibonacci: ");
 
+// Logic to calculate Fibonacci
 for (int i = 0; i < input; i++)
 {
     if (i < input - 1)
