ab0de555a69da99c147fb55fd631d3db271b797f
rkraus-dev <krobert98@outlook.de>
2026-06-11 11:30:26 +0200

    feat: add Fibonacci iterative logic

--- FibonacciIterative/Program.cs
+++ FibonacciIterative/Program.cs
@@ -1,1 +1,22 @@
-﻿Console.WriteLine("Hello, World!");
+﻿//...
+Console.Write("How many Fibonacci numbers?: ");
+int.TryParse(Console.ReadLine(), out int input);
+
+int num1 = 0;
+int num2 = 1;
+
+Console.WriteLine();
+Console.Write("Fibonacci: ");
+
+for (int i = 0; i < input; i++)
+{
+    if (i < input - 1)
+        Console.Write($"{num1}, ");
+    else
+        Console.Write($"{num1}");
+
+    int fibo = num1 + num2;
+    num1 = num2;
+    num2 = fibo;
+
+}
