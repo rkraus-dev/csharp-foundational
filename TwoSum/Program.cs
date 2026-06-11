6be3157f5a51643c270abbc3e6ea37887d5a635e
rkraus-dev <krobert98@outlook.de>
2026-06-11 11:04:02 +0200

    feat: add two sum logic

--- TwoSum/Program.cs
+++ TwoSum/Program.cs
@@ -1,1 +1,19 @@
-﻿Console.WriteLine("Hello, World!");
+﻿// ...
+int[] arr = { 2, 7, 11, 15 };
+int target = 9;
+
+// Print input
+Console.WriteLine($"[{string.Join(", ", arr)}], target = {target}");
+
+// Logic
+for(int i = 0; i < arr.Length - 1; i++)
+{
+    for(int j = i + 1; j < arr.Length; j++)
+    {
+        if(target == arr[i] + arr[j])
+        {
+            Console.WriteLine($"Indices: [{i}, {j}] ({arr[i]} + {arr[j]} = {target})");
+        }
+    }
+
+}
